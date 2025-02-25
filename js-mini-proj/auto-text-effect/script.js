c1b7e7b7d8a2f509892bbc4b9a053e19b9c943e8
ryouwq <ryoukaijuu@gmail.com>
2025-02-25 08:25:38 +0800

    add comments

--- js-mini-proj/auto-text-effect/script.js
+++ js-mini-proj/auto-text-effect/script.js
@@ -1,18 +1,24 @@
 const container = document.querySelector(".container")
 
+// array containing words that will appear 
 const careers =  ['Youtuber', 'Webdev', 'Freelancer', 'Instructor']
 
+// index of the words inside the careers array
 let careerIndex = 0
+
+// index of the characters 
 let charaIndex = 0
 
 updateText()
 
 function updateText () {
   charaIndex++
+
   container.innerHTML = 
   `
     <h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,charaIndex)}</h1>
   `
+  
   if(charaIndex === careers[careerIndex].length){
     careerIndex++
     charaIndex = 0
